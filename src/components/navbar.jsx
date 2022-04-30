2c8b3251be3fb5d625a5cdfb8ab82fed83682bb6
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-04-30 10:01:21 +0200

    make navbar component using bootstrap classes and Navlink

---
+++ src/components/navbar.jsx
@@ -0,0 +1,73 @@
+import { NavLink } from "react-router-dom";
+import React from "react";
+const Navbar = () => {
+  return (
+    <>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <div className='container-fluid'>
+          Navbar
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "red" : "black",
+                    };
+                  }}
+                  to='/home'>
+                  Home
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "red" : "black",
+                    };
+                  }}
+                  to='/login'>
+                  Login
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "red" : "black",
+                    };
+                  }}
+                  to='/register'>
+                  Register
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "red" : "black",
+                    };
+                  }}
+                  to='/users'>
+                  Users
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
