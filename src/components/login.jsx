da21e4d6d24595c54701789fb4a1fc54f5fa0b09
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-04-30 14:15:37 +0200

    add login content form with email and password ref

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,48 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { Component, createRef } from "react";
 class Login extends Component {
-  state = {};
+  email = createRef();
+  password = createRef();
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const account = {
+      email: this.email.current.value,
+      passwrod: this.password.current.value,
+    };
+    if (account.email && account.passwrod) {
+      const response = await axios.post(`https://reqres.in/api/login`, account);
+      console.log(response);
+    }
+  };
   render() {
-    return <h1>Login</h1>;
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <div className='m-3'>
+            <div className='mb-3'>
+              <label htmlFor='email'>Email:</label>
+              <input
+                ref={this.email}
+                id='email'
+                className='form-control'
+                type='text'></input>
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='password'>Password:</label>
+              <input
+                ref={this.password}
+                id='password'
+                className='form-control'
+                type='password'></input>
+            </div>
+            <button className='btn btn-primary' type='submit'>
+              Login
+            </button>
+          </div>
+        </form>
+      </>
+    );
   }
 }
 
