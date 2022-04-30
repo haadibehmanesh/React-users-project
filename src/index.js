f7689819ea2997493e1cb94f181b4d1d3313548b
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-04-30 09:59:53 +0200

    use bootstrap and react-router-dom components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import ReactDOM from "react-dom";
-//import { ReactDOM } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./app";
+import Home from "./components/home";
+import Users from "./components/users";
+import Login from "./components/login";
+import Register from "./components/register";
+import User from "./components/user";
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        <Route path="/users" element={<Users />} />
+
+        <Route path="/users/:id" element={<User />} />
+
+        <Route path="home" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+
+
+
+    </Routes>
+  </BrowserRouter>
+
+
+
+  , rootElement);
