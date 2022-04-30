6cb5ce4d35856c29f1b71b3c33e4ab09585d1beb
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-04-30 20:28:58 +0200

    use input component in login form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./input";
 class Login extends Component {
   state = {
     account: {
@@ -14,38 +15,32 @@ class Login extends Component {
   };
   handleSubmit = (event) => {
     event.preventDefault();
-    alert("A name was submitted: " + this.state.value);
   };
   render() {
     const { email, password } = this.state.account;
     return (
       <>
         <form onSubmit={this.handleSubmit}>
-          <div className='m-3'>
-            <div className='mb-3'>
-              <label htmlFor='email'>Email:</label>
-              <input
-                onChange={this.handleChange}
-                value={email}
-                name='email'
-                id='email'
-                className='form-control'
-                type='text'></input>
-            </div>
-            <div className='mb-3'>
-              <label htmlFor='password'>Password:</label>
-              <input
-                onChange={this.handleChange}
-                value={password}
-                name='password'
-                id='password'
-                className='form-control'
-                type='password'></input>
-            </div>
-            <button className='btn btn-primary' type='submit'>
-              Login
-            </button>
-          </div>
+          <Input
+            name='email'
+            label='Email'
+            value={email}
+            onChange={this.handleChange}
+            className='form-control'
+            type='text'
+          />
+          <Input
+            name='password'
+            label='Password'
+            value={password}
+            onChange={this.handleChange}
+            className='form-control'
+            type='password'
+          />
+
+          <button className='m-3 btn btn-primary' type='submit'>
+            Login
+          </button>
         </form>
       </>
     );
