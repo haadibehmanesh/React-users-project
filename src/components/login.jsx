27f98fa2523b37a5f0c04e5e67a38629183b425f
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-01 17:46:38 +0200

    add yup to login, add schema show errors

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,25 +1,73 @@
 import React, { Component } from "react";
 import Input from "./input";
+import * as yup from "yup";
+import axios from "axios";
 class Login extends Component {
   state = {
     account: {
       email: "",
       password: "",
     },
+    errors: [],
+    sending: false,
   };
+  schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Email format is not valid")
+      .required("Email field is mandatory"),
+    password: yup.string().min(4, "use at least 4 characters"),
+  });
 
   handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
     account[input.name] = input.value;
     this.setState({ account });
   };
-  handleSubmit = (event) => {
+  validate = async () => {
+    try {
+      const result = await this.schema.validate(this.state.account, {
+        abortEarly: false,
+      });
+      return result;
+    } catch (error) {
+      this.setState({ errors: error.errors });
+    }
+  };
+  handleSubmit = async (event) => {
     event.preventDefault();
+    const result = await this.validate();
+    if (result) {
+      try {
+        this.setState({ sending: true });
+
+        const response = await axios.post(
+          `https://reqres.in/api/login`,
+          result
+        );
+        this.setState({ sending: false });
+      } catch (error) {
+        this.setState({ sending: false });
+        this.setState({ errors: ["Email or password is wrong"] });
+      }
+    }
   };
   render() {
     const { email, password } = this.state.account;
     return (
       <>
+        <div className='m-3'>
+          {this.state.errors.length !== 0 && (
+            <div className='alert alert-danger'>
+              <ul>
+                {this.state.errors.map((e, i) => {
+                  return <li key={i}> {e}</li>;
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
+
         <form onSubmit={this.handleSubmit}>
           <Input
             name='email'
@@ -38,7 +86,10 @@ class Login extends Component {
             type='password'
           />
 
-          <button className='m-3 btn btn-primary' type='submit'>
+          <button
+            disabled={this.state.sending}
+            className='m-3 btn btn-primary'
+            type='submit'>
             Login
           </button>
         </form>
