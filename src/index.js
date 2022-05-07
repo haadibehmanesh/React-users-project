ecebd08c0d52114d269d5b6869e1fdd05615d414
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-07 11:58:38 +0200

    transfer routes to app component from index

--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./app";
-import Home from "./components/home";
-import Users from "./components/users";
-//import Login from "./components/login";
-import Register from "./components/register";
-import User from "./components/user";
-import NotFound from "./components/notFound";
-import Dashboard from "./components/dashboard";
-import Login from "./components/functional/login";
+import { BrowserRouter } from "react-router-dom";
+
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='*' element={<NotFound />} />
-      <Route path="/" element={<App />} >
-        <Route path="/users" element={<Users />} />
-
-        <Route path="/users/:id" element={<User />} />
-
-        <Route path="home" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
-
-
-
-    </Routes>
-  </BrowserRouter>
-
-
-
-  , rootElement);
+ReactDOM.render(<BrowserRouter> <App /></BrowserRouter>, rootElement);
