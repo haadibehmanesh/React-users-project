0f2bbd31c9cd02cf20ae219480497549b15cfd58
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-08 12:58:42 +0200

    send token in headre to api

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,26 @@
-const Dashboard = () => {
-    return (  <h1>Dashboard</h1>);
-}
- 
+import axios from "axios";
+import { useEffect } from "react";
+axios.defaults.headers.common["token"] = localStorage.getItem("token");
+const Dashboard = (props) => {
+  //const [data, setData] = useState(null);
+  useEffect(() => {
+    console.log("dashboard");
+
+    const fetchData = async () => {
+      // You can await here
+      const response = await axios.get(`https://reqres.in/api/unkown`);
+      //setData(response.data);
+      console.log("data:", response.data);
+      console.log("axios:", axios.defaults.headers.common["token"]);
+      // ...
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
+};
+
 export default Dashboard;
